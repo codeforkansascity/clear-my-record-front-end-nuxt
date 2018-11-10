c9e02c7e98209aae15911a7aa2301881051edb3c
Paul D Barham <paulb@savagesoft.com>
2018-11-09 22:37:02 -0600

    Disqualifying question now stacs correctly on narow device

--- components/disqualifying-question.vue
+++ components/disqualifying-question.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="row">
-
-
-        <div class="col-sm-4" style="margin-bottom: 2em">
+        <div class="col-sm-4 ansr" style="margin-bottom: 2em">
             <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" :name="questionname" id="inlineRadio1" value="Yes"
                        v-model="selected">
                 <label class="form-check-label" for="inlineRadio1">Yes</label>
             </div>
+
             <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" :name="questionname" id="inlineRadio2" value="No"
                        v-model="selected">
                 <label class="form-check-label" for="inlineRadio2">No</label>
             </div>
+
             <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" :name="questionname" id="inlineRadio3"
                        value="Unknown" v-model="selected">
@@ -24,21 +24,15 @@
                        v-model="selected">
                 <label class="form-check-label" for="inlineRadio4">(?)</label>
             </div>
-
         </div>
 
-        <div class="col-sm-8">
+        <div class="col-sm-8 ques">
             <slot></slot>
+            <div style="margin-left: 3em; margin-right: 2em; margin-bottom: .5em; margin-top: .5em;"
+                 v-if="selected == 'Help'">
+                <slot name="help"></slot>
+            </div>
         </div>
-
-        <div class="col-sm-6" style="margin: 2em;" v-if="selected == 'Help'">
-            <slot name="help"></slot>
-        </div>
-
-
-
-
-
     </div>
 </template>
 
@@ -58,16 +52,23 @@
         },
         watch: {
             selected: function (val) {
-                console.log(val);
-
-                if ( val == 'No') {
-  //                  this.$router.replace('cant-expunge');
-                }
+                console.log(this.questionname + ': ' + val);
             }
         }
     }
 </script>
 
 <style scoped>
+    @media screen and (max-width: 600px) {
 
+        .ansr {
+            order: 2;
+            margin-left: 1.5em;
+            margin-top: .5em;
+        }
+
+        .ques {
+            order: 1;
+        }
+    }
 </style>
--- store/index.js
+++ store/index.js
@@ -9,7 +9,6 @@ const store = () => {
             increment (state) {
                 state.counter++
             },
-
         }
     })
 }
