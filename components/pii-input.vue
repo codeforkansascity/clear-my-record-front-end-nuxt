82e69cece499ffec40ceeda0851293a2a3015a93
Paul Barham <paulb@pdblaptop.local>
2018-11-28 23:49:06 -0600

    Custom input component that saves to VueX store

--- components/pii-input.vue
+++ components/pii-input.vue
@@ -2,14 +2,57 @@
     <div class="mb-3">
         <label for="phone"><slot></slot></label>
         <div class="input-group">
-            <input type="text" class="form-control" id="phone"  required="*">
+            <input type="text"
+                   class="form-control"
+                   required="*"
+                   id="phone"
+                   :value="value"
+                   @input="updateCode($event.target.value)"
+                   ref="input">
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "pii-input"
+        name: "pii-input",
+        props: {
+            group: {
+                type: String,
+                default: 'group',
+            },
+            questionname: {
+                type: String,
+                default: 'q1',
+            }
+
+        },
+        data: function () {
+            return {
+                value: null,
+            }
+        },
+        mounted: function () {
+
+            var all = this.$store.getters.allQuestions;
+
+            var index = all.findIndex(p => p.group == this.group && p.question == this.questionname);
+
+            if (index === -1 ) {
+                this.value = null;
+            } else {
+                this.value = all[index].answer;
+            }
+
+        },
+
+        methods: {
+            updateCode(val) {
+                // Atttach validation + sanitization here.
+                this.$store.dispatch('storeQuestion', { group: this.group, question: this.questionname, answer: val });
+
+            }
+        }
     }
 </script>
 
--- pages/your-conviction.vue
+++ pages/your-conviction.vue
@@ -6,11 +6,11 @@
     </template>
 
       <form>
-          <pii-input>Was the court a Missouri circuit (county) court or a municipal (city) court?</pii-input>
-          <pii-input>What was the name of the County or City?</pii-input>
-          <pii-input>What was the case number?</pii-input>
-          <pii-input>What was the name of the Judge</pii-input>
-          <pii-input>What was your name as it appeared on the court’s records?</pii-input>
+          <pii-input group="conviction" questionname="inMo">Was the court a Missouri circuit (county) court or a municipal (city) court?</pii-input>
+          <pii-input group="conviction" questionname="city">What was the name of the County or City?</pii-input>
+          <pii-input group="conviction" questionname="case_number">What was the case number?</pii-input>
+          <pii-input group="conviction" questionname="judge_name">What was the name of the Judge</pii-input>
+          <pii-input group="conviction" questionname="your_name">What was your name as it appeared on the court’s records?</pii-input>
       </form>
 
   </sec>
