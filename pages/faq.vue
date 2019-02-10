3a10248b48a8f3be26d73bf353797525276a85c2
Paul Barham <paulb@pdblaptop.local>
2019-02-10 16:50:29 -0600

    Updated FAQ and moved content into its own section-faq.vue

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,16 +1,18 @@
-<template lang="md">
-
-
-  # FAQ
-
-  ## What is expungement
-
-
-
-
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <section-faq></section-faq>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
+    import SectionFaq from "../components/section-faq";
+
     export default {
+        components: {SectionFaq}
+
         // page component definitions
     }
 </script>
