639fbf3bfff4f35d2b71eea51e3fe4738d07c552
Paul Barham <paulb@pdblaptop.local>
2019-02-13 23:36:10 -0600

    Another version of bullted list on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,45 @@
 <template>
-        <two-panels style="padding-top: 30px;">
-            <template slot="left">
-                <h3 style="padding-bottom: 30px; font-weight: bold;">Is a crime on your record preventing you from getting a job, housing, or other things you need?</h3>
-                <p>The State of Missouri allows some criminal offenses to be expunged from your criminal record. This means that the record is closed and you are able to honestly say “no” to whether you have the conviction almost all the time. The state even provides a self-help form for you to fill out and file.</p>
-                <a class="btn btn-secondary btn-sm"
-                   href="/do-you-qualify">Find out if you qualify
-                </a>
-            </template>
-            <template slot="right">
-                <h5>THIS IS HOW IT WORKS.</h5>
-                <ol class="works-list">
-                    <li>See if you qualify for expungement</li>
-                    <li>Give us some information about you and your conviction</li>
-                    <li>We'll generate a completed application </li>
-                    <li>Print and submit the application</li>
-                </ol>
-            </template>
-        </two-panels>
+    <two-panels style="padding-top: 30px;">
+        <template slot="left">
+            <h3 style="padding-bottom: 30px; font-weight: bold;">Is a crime on your record preventing you from getting a
+                job, housing, or other things you need?</h3>
+            <p>The State of Missouri allows some criminal offenses to be expunged from your criminal record. This means
+                that the record is closed and you are able to honestly say “no” to whether you have the conviction
+                almost all the time. The state even provides a self-help form for you to fill out and file.</p>
+            <a class="btn btn-secondary btn-sm"
+               href="/do-you-qualify">Find out if you qualify
+            </a>
+        </template>
+        <template slot="right">
+            <h5>THIS IS HOW IT WORKS.</h5>
+            <div class="steps">
+                <div class="step" data-step-number="1">
+                    <h4>See if you qualify for expungement</h4>
+
+
+                </div>
+                <div class="step" data-step-number="2">
+                    <h4>Give us some information about you and your conviction</h4>
+
+
+                </div>
+                <div class="step" data-step-number="3">
+                    <h4>We'll generate a completed application</h4>
+
+
+                </div>
+                <div class="step" data-step-number="4">
+                    <h4>Print and submit the application</h4>
+
+
+                </div>
+            </div>
+        </template>
+    </two-panels>
 </template>
 
 <script>
     import TwoPanels from "../components/two-panels";
-
 
 
     export default {
@@ -31,41 +49,53 @@
 
 <style>
 
-    /* From https://stackoverflow.com/questions/17629286/css-vertical-line-between-bullets-in-an-unordered-list */
-
-    ol.works-list li {
-        position:relative; /* so that pseudoelements are positioned relatively to their "li"s*/
-        /* use padding-bottom instead of margin-bottom.*/
-        margin-bottom: 0; /* This overrides previously specified margin-bottom */
-        padding-bottom: 2.5em;
+    .steps {
+        padding: 15px;
+    }
+    .step h4 {
+        font-size: 1.2em;
+        padding-top: 1px;
+        padding-bottom: 1em;
     }
 
-    ol.works-list li:after {
-        /* bullets */
-        content: url('http://upload.wikimedia.org/wikipedia/commons/thumb/3/30/RedDisc.svg/20px-RedDisc.svg.png');
+    .step {
+        border-left: 2px dotted #C5CBD4;
+    }
+
+    .step {
+        margin-left: 18px;
+        position: relative;
+        border-left: solid 2px #6c757d;
+        padding: 0 0 1px 35px;
+    }
+
+    .step {
+        margin-left: 29px;
+        padding-left: 42px;
+    }
+
+    .step:before {
+        content: attr(data-step-number);
         position: absolute;
-        left: -26px; /*adjust manually*/
-        top: 0px;
+        border-radius: 50%;
+        width: 28px;
+        height: 28px;
+        text-align: center;
+        background-color: #ffffff;      /* Hides the line going through the circle */
+
+        border: 2px solid #6c757d;
+        color: #6c757d;             /* Color of the number ie content from above */
+        font-size: 1em;             /* Font size of the number */
+        font-weight: normal;
+        padding-top: 0px;
+        left: -15px;
+
     }
 
-    ol.works-list li:before {
-        /* lines */
-        content:"";
-        position: absolute;
-        left: -16px; /* adjust manually */
-        border-left: 1px solid black;
-        height: 100%;
-        width: 1px;
+    .step:last-child {
+        border-left: none;
+        left: 2px;
     }
 
-    ol.works-list li:first-child:before {
-        /* first li's line */
-        top: 6px; /* moves the line down so that it disappears under the bullet. Adjust manually */
-    }
-
-    ol.works-list li:last-child:before {
-        /* last li's line */
-        height: 6px; /* shorten the line so it goes only up to the bullet. Is equal to first-child:before's top */
-    }
 
 </style>
